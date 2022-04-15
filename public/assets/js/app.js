a267116c924ff16ed36d1de0655502627f3841a1
Sacha COHEN <contact@sacha-cohen.fr>
2022-04-15 09:25:17 +0200

    Update UI & js to send report request

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,6 @@
 $(function() {
+    const modal = new bootstrap.Modal($('.modal'));
+    const toastSuccess = new bootstrap.Toast($('.toast.success'));
     getPosts();
     
     function getPosts() {
@@ -34,8 +36,35 @@ $(function() {
     }
 
     $('main').on('click', '.report', function() {
-        console.log('clicked on ', $(this).parent().attr('data-id'));
-        const modal = new bootstrap.Modal($('.modal'));
+        const dataId = $(this).parent().attr('data-id');
         modal.show();
+        $('form input.post').val(dataId);
+    });
+
+    $('button.report').on('click', function() {
+        const reason = $('form input.reason').val();
+        const post = $('form input.post').val();
+
+        $.ajax({
+            url: 'http://localhost:8000/api/graphql',
+            contentType: "application/json",
+            type:'POST',
+            data: JSON.stringify({
+                query: `mutation { createReport(input: {reason: "${reason}", post: "${post}"}) { report { reason id post { id isHidden } }}}`
+            }),
+            success: function(result) {
+                modal.hide();
+                toastSuccess.show();
+                $('form input.reason').val('');
+                $('form input.post').val('');
+            },
+            error: function(error) {
+                console.log('error', error);
+            }
+        });
+    });
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
     });
 })
